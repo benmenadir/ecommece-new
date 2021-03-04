54c23a0b8bc594d4e06d02c8b164f6022578604f
BEN NADIR <benmenadirgithub@gmail.com>
2021-03-04 18:30:14 +0100

    origin dev base

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+
+var elixir = require('laravel-elixir');
+
+
+elixir.config.sourcemaps = false;
+var gulp = require('gulp');
+
+
+elixir( function (mix) {
+    var bowerPath = 'bower/vendor';
+
+    mix.sass('resources/assets/sass/app.scss', 'resources/assets/css');
+    mix.styles( //combine css file
+        [
+            'css/app.css',
+            bowerPath + '/slick-carousel/slick/slick.css'
+
+        ], 'public/css/all.css', 'resources/assets'
+    );
+    mix.scripts(
+        [
+            bowerPath + '/jquery/dist/jquery.min.js',
+            bowerPath + '/foundation-sites/dist/js/foundation.min.js',
+            bowerPath + '/slick-carousel/slick/slick.min.js'
+        ], 'public/js/all.js', 'resources/assets'
+
+    );
+
+});
